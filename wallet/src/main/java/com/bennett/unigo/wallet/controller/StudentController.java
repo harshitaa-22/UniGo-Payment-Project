9ce80f79091f9d01fd1e9f67324e879058814c2e
Rakesh Arora <rakesh.arora@ansell.com>
2024-04-13 17:07:03 +0530

    my changes:

--- wallet/src/main/java/com/bennett/unigo/wallet/controller/StudentController.java
+++ wallet/src/main/java/com/bennett/unigo/wallet/controller/StudentController.java
@@ -1,10 +1,17 @@
 package com.bennett.unigo.wallet.controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import com.bennett.unigo.wallet.entity.Student;
 import com.bennett.unigo.wallet.service.StudentService;
 
+import jakarta.servlet.http.HttpServletResponse;
+import net.glxn.qrgen.QRCode;
+import net.glxn.qrgen.image.ImageType;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -37,4 +44,20 @@ public class StudentController {
     public void deleteStudent(@PathVariable Long id) {
         studentService.deleteStudent(id);
     }
+    
+    @GetMapping(value = "/qrcode/{id}", produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] generateQRCode(@PathVariable("id") String id, HttpServletResponse response) throws IOException {
+        // Generate the QR code based on the ID
+        ByteArrayOutputStream outputStream = QRCode.from(id).withSize(250, 250).to(ImageType.PNG).stream();
+
+        // Set content type
+        response.setContentType(MediaType.IMAGE_PNG_VALUE);
+
+        // Write the QR code image to the response
+        response.getOutputStream().write(outputStream.toByteArray());
+        response.getOutputStream().flush();
+
+        return outputStream.toByteArray();
+    }
+    
 }
